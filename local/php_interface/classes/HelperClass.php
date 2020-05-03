e8c46e78b8787bf0389f7a1e2fd33a59ad4abb30
MikhailBrovko <mikhail.brovko@rutorika.ru>
2020-05-03 10:48:39 +0300

    update for psr-4

--- local/php_interface/classes/HelperClass.php
+++ local/php_interface/classes/HelperClass.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Classes\Helpers;
+namespace MichaelBro;
 
-class Other
+class HelperClass
 {
     /**
      * Возвращает форму слова в зависимости от числа
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,8 +2,6 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/handlers/sale.php';
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/helpers/other.php';
-
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php')) {
     require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 }
